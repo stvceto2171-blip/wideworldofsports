55de4dcb5bbeb926c23f1de6f0de66d1c09356cc
stvceto2171-blip <stvceto2171@gmail.com>
2025-10-22 16:46:56 -0700

    check it

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,15 +1,20 @@
-// This is a next.js server action, which is an alpha feature, so
-// use with caution.
+"use server";
+
+import { addReviewToRestaurant } from "@/src/lib/firebase/firestore.js";
+import { getAuthenticatedAppForUser } from "@/src/lib/firebase/serverApp.js";
+import { getFirestore } from "firebase/firestore";
+
+// This is a Server Action
 // https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
 export async function handleReviewFormSubmission(data) {
-        const { app } = await getAuthenticatedAppForUser();
-        const db = getFirestore(app);
+  const { firebaseServerApp } = await getAuthenticatedAppForUser();
+  const db = getFirestore(firebaseServerApp);
 
-        await addReviewToRestaurant(db, data.get("restaurantId"), {
-                text: data.get("text"),
-                rating: data.get("rating"),
+  await addReviewToRestaurant(db, data.get("restaurantId"), {
+    text: data.get("text"),
+    rating: data.get("rating"),
 
-                // This came from a hidden form field.
-                userId: data.get("userId"),
-        });
+    // This came from a hidden form field
+    userId: data.get("userId"),
+  });
 }
